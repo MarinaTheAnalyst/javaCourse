fb2c6bd8aa459e10292b69445ce983f7a67e2a13
MarinaTheAnalyst <marina97gontaryk@gmail.com>
2022-02-06 21:49:12 +0000

    java script cw3

---
+++ java_classwork/cw3Java.js
@@ -0,0 +1,131 @@
+//1. зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та: перебрати його циклом while
+
+ let arr = [2,17,13,6,22,31,45,66,100,-18];
+ // let i = 0;
+ //    while (i<arr.length) {
+ //     console.log(arr[i]);
+ //     i++;
+ //    }
+
+//2. перебрати його циклом for
+
+       // for (const cell of arr) {
+       //  console.log(cell);
+       //  }
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     console.log(arr[i]);
+    //
+    // }
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+    // let i = 0;
+    //    while (i<arr.length) {
+    //     if (arr[i] % 2 === 1) {
+    //      console.log(arr[i]);
+    //     };
+    //     i++
+    //    }
+
+
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] % 2 === 1) {
+    //         console.log(arr[i]);
+    //     }
+    // }
+
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+
+
+// let i = 0;
+//    while (i<arr.length) {
+//     if (arr[i] % 2 === 0) {
+//      console.log(arr[i]);
+//     };
+//     i++
+//    }
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+//             for (let i = 0; i < arr.length; i++) {
+//                 if (arr[i] % 2 === 0) {
+//                     console.log(arr[i]);
+//                 }
+//             }
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+
+                // for (let i = 0; i < arr.length; i++) {
+                //     if (arr[i] % 3 === 0)
+                //     {  arr[i] = "okten";
+                //         console.log(arr[i]);
+                //     }
+                // }
+
+// 8. вивести масив в зворотньому порядку.
+
+            // for (let i = arr.length - 1; i >= 0; i--) {
+            //     console.log(arr[i]);
+            // }
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+    //9.1
+    //         let i = arr.length - 1;
+    //            while (i >= 0) {
+    //             console.log(arr[i]);
+    //             i--;
+    //            }
+
+//9.2
+            // for (let i = arr.length - 1; i >= 0; i--) {
+            //     console.log(arr[i]);
+            // }
+
+//9.3
+//         let i = arr.length - 1;
+//            while (i >= 0) {
+//                {
+//                if (arr[i] % 2 === 1) {
+//                    console.log(arr[i]);
+//                };
+//            }
+//             i--;
+//            }
+
+
+//9.4
+//             for (let i = arr.length - 1; i >= 0; i--) {
+//                 { if (arr[i] % 2 === 1)
+//                 {console.log(arr[i]);}
+//                 };
+//
+//             }
+
+
+//9.5
+                // let i = 0;
+                //    while (i<arr.length) {
+                //     if (arr[i] % 2 === 0) {
+                //      console.log(arr[i]);
+                //     };
+                //     i++
+                //    }
+
+//9.6
+//             for (let i = arr.length - 1; i >= 0;  i--) {
+//                 if (arr[i] % 2 === 0) {
+//                     console.log(arr[i]);
+//                 }
+//             }
+
+//9.7
+                for (let i = arr.length - 1; i >= 0; i--) {
+                    if (arr[i] % 3 === 0)
+                    {  arr[i] = "okten";
+                        console.log(arr[i]);
+                    }
+                }
+
