472b9ea5b2c70865e92a6ef8ea33c6c29be60c62
MarinaTheAnalyst <marina97gontaryk@gmail.com>
2022-02-16 23:21:53 +0000

    adding hm7

--- java_homework/hm7JS.js
+++ java_homework/hm7JS.js
@@ -48,20 +48,20 @@ let funcSort = user.sort((a,b) => a.id - b.id); // ((a,b) => b.id - a.id);
     }
 
     let database = [
-        new Client(9, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 52),
-        new Client(10, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 12),
-        new Client(15, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 34),
-        new Client(13, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 350),
-        new Client(23, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 120),
-        new Client(21, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 235),
-        new Client(11, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 333),
-        new Client(5, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 41),
-        new Client(3, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 65),
-        new Client(25, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, 22)
+        new Client(9, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['phone']),
+        new Client(10, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['case', 'headphones']),
+        new Client(15, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['computer', 'iPad', 'iWatch']),
+        new Client(13, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['case']),
+        new Client(23, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['headphones']),
+        new Client(21, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['JBL']),
+        new Client(11, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['speaker']),
+        new Client(5, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['laptop']),
+        new Client(3, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['phone', 'case', 'iPad']),
+        new Client(25, 'Arthur', 'Connan', 'arcon@gmail.com', 2345678, ['case', 'headphones'])
     ];
     console.log(database);
 
 //5. - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 
-    let sortFunc = database.sort((a,b) => a.order - b.order);
+    let sortFunc = database.sort((a,b) => a.order.length - b.order.length);
     console.log(sortFunc);
